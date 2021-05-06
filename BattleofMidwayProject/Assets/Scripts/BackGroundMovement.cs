f1946dc2b5af4646c0d7a7f5bd214ee7e67022a1
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-05-06 19:19:37 -0300

    projetoInicial

---
+++ BattleofMidwayProject/Assets/Scripts/BackGroundMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMovement : MonoBehaviour
+{
+    public float _BackgroundSpeed;
+    
+
+  
+    void Update()
+    {
+        transform.Translate(0, _BackgroundSpeed * Time.deltaTime, 0);
+    }
+}
