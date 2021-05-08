33642c1d147312fdb84c004b4866eee885186331
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-05-08 16:31:20 -0300

    comecoPowerUps

---
+++ BattleofMidwayProject/Assets/Scripts/powerUps/PowerUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class PowerUp 
+{
+    [SerializeField]
+    public string _name;
+
+    [SerializeField]
+    public int _type;
+
+    [SerializeField]
+    public float _dropChance;
+
+    [SerializeField]
+    public float _duration;
+
+    [SerializeField]
+    public UnityEvent _startAction;
+
+    [SerializeField]
+    public UnityEvent _endAction;
+
+    [SerializeField]
+    public UnityEvent _dropAction;
+
+    public void Start()
+    {
+        if (_startAction != null)
+        {
+            _startAction.Invoke();
+        }
+    }
+
+    public void End()
+    {
+        if(_endAction != null)
+        {
+            _endAction.Invoke();
+        }
+    }
+
+    public void Drop()
+    {
+        if (_dropAction != null)
+        {
+            _dropAction.Invoke();
+        }
+    }
+
+}
---
+++ BattleofMidwayProject/Assets/Scripts/powerUps/PowerUpActions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpActions : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController _playerController;
+    [SerializeField]
+    private PlayerBullet _playerBullet;
+
+    public void IncreaseSpeed()
+    {
+        _playerController._moveSpeed += 2;
+    }
+    public void IncreaseFireRate()
+    {
+        _playerBullet.fireInterval -= 0.2f;
+    }
+}
